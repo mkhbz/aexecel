04e4dba259b89aedcf3ab29809386aecae1a3e0e
hushilin <hushilin@sugo.io>
2020-09-01 17:42:20 +0800

    feat:添加eslint

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+console.log('开始')
+// ie浏览器支持使用activeXoBJECT
+function isIE(): Boolean {
+  return window.ActiveXObject ? true : false
+}
+// 导出方法
+export default function JSON2SCV() {
+  if (!isIE()) {
+
+    aTagExport()
+
+  }
+  // 对activeXobject的处理
+  activeXObjectExport()
+}
+// activeXobject
+function activeXObjectExport() {
+
+}
+// a标签
+function aTagExport() {
+  const aElement: HTMLAnchorElement = document.createElement("a");
+  document.querySelector('body').appendChild(aElement);
+}
